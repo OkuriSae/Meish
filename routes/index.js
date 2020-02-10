2e63b7e63bda1827c758e6c55bba812dc4538352
OkuriSae <sae.okuri@gmail.com>
2020-02-10 15:26:09 +0900

    add light shuffle, add view all characters

--- routes/index.js
+++ routes/index.js
@@ -11,33 +11,12 @@ const authenticationEnsurer = require('./authentication-ensurer');
 
 router.get('/', (req, res, next) => {
   (async () => {
-    const results = await database.query(`
-      SELECT "username", "thumbnail_path", "isSensitive"
-      FROM "users" 
-        JOIN "personalities" ON "users"."userId" = "personalities"."userId"
-      WHERE
-        "visibility" = 1
-        AND "thumbnail_path" is not null
-        ORDER BY random()
-      LIMIT 30
-      `, { type: Sequelize.QueryTypes.SELECT });
-    let tagSummary = await Tag.findAll({
-      attributes: [
-        'tagname',
-        [Sequelize.fn('count', Sequelize.col('tagname')), 'count']
-      ],
-      group: ["tagname"],
-      order: [ [Sequelize.fn('count', Sequelize.col('tagname')), 'DESC' ] ],
-      limit: '300'
-    });
-    let userCount = await User.count();
-    
     res.render('index', {
       me: req.user,
       s3: process.env.s3Path,
-      results: results,
-      tags: tagSummary,
-      userCount: userCount
+      results: await getRandomUsers(),
+      tags: await getTags(),
+      userCount: await User.count()
     });
   })();
 });
@@ -45,38 +24,73 @@ router.get('/', (req, res, next) => {
 router.get('/search', (req, res, next) => {
   let query = req.query.query;
   (async () => {
-    const results = await database.query(`
-      SELECT DISTINCT "username", "thumbnail_path", "isSensitive"
-      FROM "users" 
-        JOIN "personalities" ON "users"."userId" = "personalities"."userId"
-        JOIN "tags" ON "users"."userId" = "tags"."userId"
-      WHERE
-        "visibility" = 1
-        AND "tagname" ilike $tagname
-        AND "thumbnail_path" is not null
-      LIMIT 30
-      `
-      , {
-      bind: {tagname: query},
-      type: Sequelize.QueryTypes.SELECT
-    });
-    let tagSummary = await Tag.findAll({
-      attributes: [
-        'tagname',
-        [Sequelize.fn('count', Sequelize.col('tagname')), 'count']
-      ],
-      group: ["tagname"],
-      order: [ [Sequelize.fn('count', Sequelize.col('tagname')), 'DESC' ] ],
-      limit: '300'
-    });
     res.render('index', {
       me: req.user,
       s3: process.env.s3Path,
-      results: results,
-      tags: tagSummary
+      results: await getRandomUsers(query),
+      tags: await getTags()
     });
   })();
 });
+
+router.get('/all', (req, res, next) => {
+  let query = req.query.query;
+  (async () => {
+    res.render('index', {
+      me: req.user,
+      s3: process.env.s3Path,
+      results:await getAllUsers(query, 'latest'),
+      tags: await getTags(),
+      userCount: await User.count()
+    });
+  })();
+});
+
+async function getAllUsers(query, order) {
+  // sequelizeでうまくかけないのでベタがき
+  return database.query(`
+    SELECT DISTINCT "username", "thumbnail_path", "isSensitive", "users"."createdAt"
+    FROM "users" 
+      JOIN "personalities" ON "users"."userId" = "personalities"."userId"
+      ${ query ? `JOIN "tags" ON "users"."userId" = "tags"."userId"` : "" }
+    WHERE
+      "visibility" = 1
+      ${ query ? `AND "tagname" ilike $tagname` : "" }
+      AND "thumbnail_path" is not null
+    ${ order == 'latest' ? `ORDER BY "users"."createdAt" desc` : "" }
+    `
+    , {
+    bind: {tagname: query},
+    type: Sequelize.QueryTypes.SELECT
+  });
+}
+
+async function getRandomUsers(query) {
+
+  let users = await getAllUsers(query);
+
+  // Fisher–Yates Shuffle
+  for(var i = users.length - 1; i > 0; i--){
+    var r = Math.floor(Math.random() * (i + 1));
+    var tmp = users[i];
+    users[i] = users[r];
+    users[r] = tmp;
+  }
+  
+  return users.slice(0, 30);
+}
+
+async function getTags() {
+  return Tag.findAll({
+    attributes: [
+      'tagname',
+      [Sequelize.fn('count', Sequelize.col('tagname')), 'count']
+    ],
+    group: ["tagname"],
+    order: [ [Sequelize.fn('count', Sequelize.col('tagname')), 'DESC' ] ],
+    limit: '300'
+  });
+}
 
 router.get('/about', (req, res, next) => {
   res.render('about', { me: req.user });
