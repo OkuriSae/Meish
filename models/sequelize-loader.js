625da481960ab05d3e28906fe2f7daa37d47f59e
OkuriSae <sae.okuri@gmail.com>
2020-04-01 01:23:22 +0900

    management conection pool

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -4,7 +4,13 @@ const sequelize = new Sequelize(
   process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/meishdev',
   {
     operatorsAliases: false,
-    logging: false 
+    logging: false,
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 20000,
+      acquire: 20000
+    }
   });
 
 module.exports = {
